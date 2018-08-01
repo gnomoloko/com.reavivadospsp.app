03621f68d0f4d33baf9a15b0b19b10b59df02bb1
gnomoloko <gnomoloko@gmail.com>
2018-07-31 22:34:10 -0300

    version-3.4---31-07-2018

--- src/homeIcon.js
+++
@@ -1,9 +0,0 @@
-<View style={styles.homeIcon}>
-          <TouchableOpacity onPress={()=>{this.props.navigation.navigate}}/>
-            <Image source={require ('../img/home.png')} style={{width:24 , height:24}} />
-          </TouchableOpacity>
- </View>
-
-
-
-
